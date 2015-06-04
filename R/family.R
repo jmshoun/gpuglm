b91bf708004239be1b01786e3c54a91ce52cb34e
mark <mark.shoun@gmail.com>
2015-06-03 22:21:48 -0400

    Added family argument to gpuglm_family to align with family, checked parameters on R side

--- R/family.R
+++ R/family.R
@@ -1,17 +1,26 @@
+.CANONICAL_LINKS <- data.frame(family=c('gaussian', 'poisson', 'binomial', 'Gamma', 
+                                             'inverse.gaussian', 'negative.binomial'),
+                               link=c('identity', 'log', 'logit', 'reciprocal', 
+                                      'squared reciprocal', 'negative binomial'),
+                               variance=c('constant', 'identity', 'binomial',  'squared', 'cubed',
+                                          'negative binomial'))
+
 ##' GPU-GLM Family Object
 ##' 
 ##' \code{gpuglm_family} is the R-side object that contains details about the family to be use in a
 ##' generalized linear model.
 ##' 
 ##' The family for a GLM is defined as the combination of the link and variance functions 
-##' assocaiated with the model. (There is also an inverse link function, but it is deteremined by
+##' associated with the model. (There is also an inverse link function, but it is deteremined by
 ##' the link function.)
 ##' 
-##' Please note: There is no validation of the link and variance function strings supplied by the
-##' user. If the link or variance function strings are undefined in the C++/CUDA code, the (silent)
-##' fallback link and variance functions are both \code{'identity'}.
+##' Please note: passing a family name to this function will automatically override any supplied
+##' \code{link} and \code{variance} values.
 ##' 
-##' @param link A case-insentive string with the name of the link function. The following link
+##' @param family A string with the name of the family. All of the standard \code{glm} families
+##' are defined except for the \code{quasi} families. The \code{negative.binomial} family is also
+##' defined.
+##' @param link A case-insensitive string with the name of the link function. The following link
 ##' functions are currently defined:
 ##' \itemize{
 ##'   \item \code{identity}
@@ -32,8 +41,29 @@
 ##'   \item \code{negative binomial}
 ##' }
 ##' @return An object of class \code{gpuglm_family}.
-gpuglm_family <- function(link='identity', variance='constant') {
+gpuglm_family <- function(family, link='identity', variance='constant') {
+  if (!missing(family)) {
+    .validate_family_parameter(family, 'family')
+    
+    link <- .CANONICAL_LINKS[.CANONICAL_LINKS$family == family, 'link']
+    variance <- .CANONICAL_LINKS[.CANONICAL_LINKS$family == family, 'variance']
+  }
+  
+  .validate_family_parameter(link, 'link')
+  .validate_family_parameter(variance, 'variance')
+  
   structure(list(link=tolower(link),
                  variance=tolower(variance),
+                 canonical=.determine_canonicity(link, variance),
                  class='gpuglm_family'))
+}
+
+.validate_family_parameter <- function(parameter, parameter.name) {
+  if (!(parameter %in% .CANONICAL_LINKS[[parameter.name]])) {
+    stop('Unrecognized ', parameter.name, ' name')
+  }
+}
+
+.determine_canonicity <- function(link, variance) {
+  which(.CANONICAL_LINKS$link == link) == which(.CANONICAL_LINKS$variance == variance)
 }
