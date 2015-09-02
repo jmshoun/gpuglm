7d6d0c156d7700c3ca4878cb6c8fefc00c13a213
mark <mark.shoun@gmail.com>
2015-09-01 21:28:31 -0400

    Cleaned up some edge cases with print method.

--- R/glm_print.R
+++ R/glm_print.R
@@ -9,10 +9,12 @@ print.gpuglm <- function(x, ...) {
   cat('\nNumeric Coefficients:\n')
   print(c(x$beta$intercept, x$beta$numeric))
   
-  cat('\nFactor Coefficeints:\n')
-  for (i in 1:length(x$beta$factor)) {
-    cat(names(x$beta$factor)[i], '\n', sep='')
-    print(x$beta$factor[[i]])
+  if (!is.null(x$beta$factor)) {
+    cat('\nFactor Coefficients:\n')
+    for (i in 1:length(x$beta$factor)) {
+      cat(names(x$beta$factor)[i], '\n', sep='')
+      print(x$beta$factor[[i]])
+    }
   }
   
   cat('\nResidual Deviance: ', -2 * x$log.likelihood, 
